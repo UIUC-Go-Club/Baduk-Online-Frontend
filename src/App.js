f4fd2befcbfdb088c6c5a1a957255d08e6e3d170
zyu18 <zyu18@illinois.edu>
2020-11-07 17:13:14 -0600

    board with switch play

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Goban } from '@sabaki/shudan'
+import '@sabaki/shudan/css/goban.css'
+import '@sabaki/go-board'
+import GoBoard from '@sabaki/go-board';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const signMap = [
+    [ 0, 0, 0,-1,-1,-1, 1, 0, 1, 1,-1,-1, 0,-1, 0,-1,-1, 1, 0],
+    [ 0, 0,-1, 0,-1, 1, 1, 1, 0, 1,-1, 0,-1,-1,-1,-1, 1, 1, 0],
+    [ 0, 0,-1,-1,-1, 1, 1, 0, 0, 1, 1,-1,-1, 1,-1, 1, 0, 1, 0],
+    [ 0, 0, 0, 0,-1,-1, 1, 0, 1,-1, 1, 1, 1, 1, 1, 0, 1, 0, 0],
+    [ 0, 0, 0, 0,-1, 0,-1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+    [ 0, 0,-1, 0, 0,-1,-1, 1, 0,-1,-1, 1,-1,-1, 0, 1, 0, 0, 1],
+    [ 0, 0, 0,-1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1,-1,-1, 1, 1, 1],
+    [ 0, 0,-1, 1, 1, 0, 1,-1,-1, 1, 0, 1,-1, 0, 1,-1,-1,-1, 1],
+    [ 0, 0,-1,-1, 1, 1, 1, 0,-1, 1,-1,-1, 0,-1,-1, 1, 1, 1, 1],
+    [ 0, 0,-1, 1, 1,-1,-1,-1,-1, 1, 1, 1,-1,-1,-1,-1, 1,-1,-1],
+    [-1,-1,-1,-1, 1, 1, 1,-1, 0,-1, 1,-1,-1, 0,-1, 1, 1,-1, 0],
+    [-1, 1,-1, 0,-1,-1,-1,-1,-1,-1, 1,-1, 0,-1,-1, 1,-1, 0,-1],
+    [ 1, 1, 1, 1,-1, 1, 1, 1,-1, 1, 0, 1,-1, 0,-1, 1,-1,-1, 0],
+    [ 0, 1,-1, 1, 1,-1,-1, 1,-1, 1, 1, 1,-1, 1,-1, 1, 1,-1, 1],
+    [ 0, 0,-1, 1, 0, 0, 1, 1,-1,-1, 0, 1,-1, 1,-1, 1,-1, 0,-1],
+    [ 0, 0, 1, 0, 1, 0, 1, 1, 1,-1,-1, 1,-1,-1, 1,-1,-1,-1, 0],
+    [ 0, 0, 0, 0, 1, 1, 0, 1,-1, 0,-1,-1, 1, 1, 1, 1,-1,-1,-1],
+    [ 0, 0, 1, 1,-1, 1, 1,-1, 0,-1,-1, 1, 1, 1, 1, 0, 1,-1, 1],
+    [ 0, 0, 0, 1,-1,-1,-1,-1,-1, 0,-1,-1, 1, 1, 0, 1, 1, 1, 0]
+]
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: new GoBoard(signMap),
+            currPlayer : 1
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    mouseClick = (evt, [x, y]) => {
+        let sign = this.state.currPlayer;
+        try {
+            let newBoard = this.state.board.makeMove(sign, [x, y], {preventOverwrite:true})
+            this.setState({
+                board: newBoard,
+                currPlayer: this.state.currPlayer * -1
+            })
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    getPlayer = () => {
+        if (this.state.currPlayer === 1) {
+            return "Black";
+        } else {
+            return 'White';
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Goban vertexSize={24} signMap={this.state.board.signMap} onVertexMouseUp={this.mouseClick}/>
+                <div>
+                    Current Player is {this.getPlayer()}
+                </div>
+            </div>
+        );
+    }
 }
-
 export default App;
